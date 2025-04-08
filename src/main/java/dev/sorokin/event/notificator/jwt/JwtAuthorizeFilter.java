184226f21e1c79ea3177fe109a050f8847c267fe
ultimathule1 <ranel28021998@gmail.com>
2025-04-08 10:37:23 +0300

    little changes for final project

--- src/main/java/dev/sorokin/event/notificator/jwt/JwtAuthorizeFilter.java
+++ src/main/java/dev/sorokin/event/notificator/jwt/JwtAuthorizeFilter.java
@@ -4,7 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.hc.core5.http.HttpHeaders;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
